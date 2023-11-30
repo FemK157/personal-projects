599975fe9db84b1dfd8aa2ed2307b89c346c05a9
FemK157 <144396161+FemK157@users.noreply.github.com>
2023-11-30 17:12:42 -0500

    Update artfinder.py

--- artfinder.py
+++ artfinder.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import os
 import requests
-import random
 
 def welcomePage():
     st.markdown("<img style='text-align: center; display: block; margin-left: auto; margin-right: auto; width: 75%; border: 10px solid #bdfbff' src='https://cdn-imgix.headout.com/media/images/a4d93bc58c9528951ed3124f77d268e4-544-amsterdam-003-amsterdam-%7C-rijksmuseum-02.jpg?auto=format&w=814.9333333333333&h=458.4&q=90&fit=crop&ar=16%3A9'>", unsafe_allow_html=True)
@@ -12,10 +10,9 @@ def welcomePage():
 def artist(timePeriod, firstLetter, pieceLetter):
     r = requests.get("https://www.rijksmuseum.nl/api/en/collection?key=63N8gLOd")
     data = r.json()
-    artistList = []
     artpieceList = []
     for artist in data["facets"][0]["facets"]:
-        if artist["key"][0] == firstLetter.upper():
+        if artist["key"][0].upper() == firstLetter.upper():
             artistList.append(artist["key"])
     for a in artistList:
         try:
@@ -29,6 +26,19 @@ def artist(timePeriod, firstLetter, pieceLetter):
     return artpieceList
 
 
+def checkTwo(artpieceNums):
+    for i, number in enumerate(artpieceNums):
+        r = requests.get("https://www.rijksmuseum.nl/api/en/collection/" + number + "?key=63N8gLOd")
+        data = r.json()
+        error = True
+        for item in data['artObject']['makers']:
+            if item['name'] in artistList:
+                error = error and False
+        if error == False:
+            artpieceNumsC.append(artpieceNums[i])
+            artpieceTitlesC.append(artpieceTitles[i])
+        
+    
 def displayInfo(number):
     r = requests.get("https://www.rijksmuseum.nl/api/en/collection/" + number + "?key=63N8gLOd")
     data = r.json()
@@ -39,7 +49,12 @@ def displayInfo(number):
         st.markdown("<img style='text-align: center; display: block; margin-left: auto; margin-right: auto; width: 75%; border: 10px solid #d1b736; margin-bottom: 20px' src='{}'>".format(url), unsafe_allow_html=True)
     except:
         st.markdown("<img style='text-align: center; display: block; margin-left: auto; margin-right: auto; width: 75%' src='https://st4.depositphotos.com/14953852/24787/v/450/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg'>", unsafe_allow_html=True)
-    artist = data['artObject']['principalMakers'][0]['name']
+    artist = ""
+    for item in data['artObject']['makers']:
+        if item['name'] in artistList:
+            artist = item['name']
+    if artist == "":
+        artist = "Can't be found"
     yearMade = data['artObject']['dating']['presentingDate']
     location = ""
     for item in data['artObject']['productionPlaces']:
@@ -57,7 +72,7 @@ def displayInfo(number):
     for item in data['artObject']['materials']:
         materials += item + ", "
     materials = materials[:-2]    
-    st.markdown("<p style='text-align: center; font-size: 20px; color: black'>Primary Artist: {}</p>".format(artist), unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center; font-size: 20px; color: black'>Artist: {}</p>".format(artist), unsafe_allow_html=True)
     st.markdown("<p style='text-align: center; font-size: 20px; color: black'>Year(s) Created: {}</p>".format(yearMade), unsafe_allow_html=True)
     st.markdown("<p style='text-align: center; font-size: 20px; color: black'>Location(s) of Production: {}</p>".format(location), unsafe_allow_html=True)
     st.markdown("<p style='text-align: center; font-size: 20px; color: black'>Artwork Type(s): {}</p>".format(types), unsafe_allow_html=True)
@@ -76,28 +91,27 @@ pieceLetter = st.sidebar.text_input("Title Contains Letter:", max_chars=1)
 
 if firstLetter != "" and pieceLetter != "":
     loadingGIF = st.markdown("<img style='text-align: center; display: block; margin-left: auto; margin-right: auto; width: 75%' src='https://cdn.dribbble.com/users/11609495/screenshots/18251844/media/a4d3556d8b51796968cbcc63ea7c5abc.gif'>", unsafe_allow_html=True)
+    artistList = []
     artpieceList = artist(timePeriod, firstLetter, pieceLetter)
     artpieceTitles = []
     artpieceNums = []
+    artpieceTitlesC = []
+    artpieceNumsC = []
     for title, num in artpieceList:
         artpieceTitles.append(title)
         artpieceNums.append(num)
+    checkTwo(artpieceNums)
     try:
-        artPiece = st.sidebar.radio("Choose an Art Piece", artpieceTitles)
-        index = artpieceTitles.index(artPiece)
-        number = artpieceNums[index]
+        artPiece = st.sidebar.radio("Choose an Art Piece", artpieceTitlesC)
+        index = artpieceTitlesC.index(artPiece)
+        number = artpieceNumsC[index]
         loadingGIF.empty()
         displayInfo(number)
     except:
         loadingGIF.empty()
         displayErrorMsg()
-    
-
 else:
     welcomePage()
-
-
-
 
 
 
